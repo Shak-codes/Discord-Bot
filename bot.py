fac5ef4b92ee458ed85e1668eddc943dfd94fe86
Shak-codes <69654263+Shak-codes@users.noreply.github.com>
2020-08-16 19:14:42 -0400

    Created the basis for a search command
    .steam_search (game) Allows a user to find the details of a game within the steam store. (Still many parameters to adjust)

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import linecache2
 import random
+import pandas as pd
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 from discord.ext import commands
@@ -83,5 +84,73 @@ async def steam(ctx, a, b):
 
     else:
         await ctx.send("Please enter valid numbers from 1 to " + str(total_games))
+
+@client.command()
+async def steam_search(ctx, raw_game):
+
+    game = raw_game.replace(" ", "+")
+
+    my_url = "https://store.steampowered.com/search/?term=" + game
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    page_soup = soup(page_html, "html.parser")
+
+    container_name = page_soup.find_all('span', attrs={'class': 'title'})
+    container_name_text = container_name[0].text
+
+    if raw_game in container_name_text:
+        x = raw_game
+    else:
+        x = container_name[0].text
+
+    steam = pd.read_csv("steam.csv")
+
+    x = steam.loc[steam["name"] == x]
+    x = x.iloc[0, 0]
+
+    my_url = "https://store.steampowered.com/app/" + str(x)
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    page_soup = soup(page_html, "html.parser")
+
+    container_game = page_soup.find_all('div', attrs={'class': 'page_content_ctn'})
+    container = container_game[0]
+
+    container_name2 = container.find_all('div', attrs={'class': 'apphub_AppName'})
+    name = container_name2[0].text
+
+    container_dev = container.find_all('div', attrs={'class': 'apphub_AppName'})
+    container_image = container.find('img').get('src')
+
+    container_banner = container.find('img', attrs={'class': 'game_header_image_full'}).get('src')
+
+    container_description = container.find_all('div', attrs={'class': 'game_description_snippet'})
+    description = container_description[0].text
+
+    container_review = container.find_all('span', attrs={'class': 'game_review_summary positive'})
+    review = container_review[0].text
+
+    container_tags = container.find_all('a', attrs={'class': 'app_tag'})
+    tags = container_tags[0].text.strip() + "\n" + container_tags[1].text.strip() + "\n" + container_tags[2].text.strip()
+
+    #container_price = container.find_all('div', attrs={'class': 'game_purchase_price price'})
+    #price = container_price[0].text
+
+    embed = discord.Embed(
+        title = name,
+        url = my_url,
+        description = description,
+        colour = discord.Colour.green()
+    )
+
+    embed.set_image(url=container_banner)
+    embed.set_thumbnail(url=container_image)
+    embed.add_field(name='Reviews', value=review)
+    embed.add_field(name='Tags', value=tags)
+    #embed.add_field(name='Price', value=price, inline=False)
+
+    await ctx.send(embed=embed)
 
 client.run(key.key)
